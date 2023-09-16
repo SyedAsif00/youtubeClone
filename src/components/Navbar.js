df4bc598fab9adafd8687fb95b3883be3252d5fb
Syed Asifullah <syedasifullah00@gmail.com>
2023-09-16 12:04:01 +0500

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,25 @@
 import React from "react";
-
-const Navbar = () => {
-  return <div>navbar</div>;
-};
-
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import { logo } from "./utils/Constants";
+import SearchBar from "./SearchBar";
+const Navbar = () => (
+  // stack is a container from mui. that has its own styling props.
+  <Stack
+    direction="row"
+    alignItems="center"
+    p={2}
+    sx={{
+      position: "sticky",
+      background: "#000",
+      top: 0,
+      justifyContent: "space-between",
+    }}
+  >
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} height={45} />
+    </Link>
+    <SearchBar />
+  </Stack>
+);
 export default Navbar;
